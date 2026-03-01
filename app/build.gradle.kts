5645f83532929ed787e71e774bd4ec22c6ea902c
Nutrient <>
2026-03-01 18:48:17 +0000

    Update example to Nutrient for Android 11.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         minSdk = 26
         targetSdk = compileSdk
 
-        versionName = "11.1.0"
-        versionCode = 146293
+        versionName = "11.1.1"
+        versionCode = 146310
 
         vectorDrawables {
             useSupportLibrary = true
@@ -70,11 +70,11 @@ dependencies {
 
     // Nutrient is integrated from the Nutrient Maven repository. See the `repositories` block at the beginning
     // of this file, which shows how to set up the repository in your app.
-    implementation("io.nutrient:nutrient:11.1.0")
+    implementation("io.nutrient:nutrient:11.1.1")
 
     // OCR library + English language pack.
-    implementation("io.nutrient:nutrient-ocr:11.1.0")
-    implementation("io.nutrient:nutrient-ocr-english:11.1.0")
+    implementation("io.nutrient:nutrient-ocr:11.1.1")
+    implementation("io.nutrient:nutrient-ocr-english:11.1.1")
 
 
     // Androidx
